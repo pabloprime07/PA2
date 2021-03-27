5aab28b1b92ec758ac62ed30e476d11c7416229c
pabloprime07 <pabloprime07@gmail.com>
2021-03-27 14:13:06 +0530

    monitor client updated

--- client.py
+++ client.py
@@ -10,26 +10,51 @@ parser = argparse.ArgumentParser()
 parser.add_argument('load', type=int, help='indicator for load on the servers')
 args = parser.parse_args()
 # print(args.load)
-
+load = args.load
 servers = list()
+servers.append('192.168.122.100')
+
+# thread function 
+def threaded(c): 
+    while True: 
+        data = c.recv(1024) 
+        if not data: 
+            print('Conn lost to monitor...') 
+            break
+  
+        print(data)
+        servers.append(data) 
+        print(data)
+        c.send(data.encode()) 
+
+    c.close() 
+
+
 
 def maintain_servers():
     host = "" 
     port = 23456
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     s.bind((host, port)) 
+    s.listen(5) 
+    print("Socket is listening") 
+  
     while True: 
-        s.listen(5)
-        c, addr = s.accept()
-        data = c.recv(1024) 
-        if not data:  
-            break
-        print(data)
-        servers.append(data)       
-        # print(data)
-        c.send(data.encode()) 
-        c.close() 
+  
+        c, addr = s.accept() 
+  
+        print('Connected to :', addr[0], ':', addr[1]) 
  
+        start_new_thread(threaded, (c,)) 
+    s.close() 
+ 
+def loadinp():
+    global load
+    while True:
+        x = input()
+        load = int(x)
+        print("New load = "+str(load))
+        time.sleep(1)
 
 def Main(): 
     # local host IP '127.0.0.1' 
@@ -38,23 +63,28 @@ def Main():
   
     # Define the port on which you want to connect 
     
-  
-    start_new_thread()
+    th1 = threading.Thread(target=maintain_servers)
+    th2 = threading.Thread(target=loadinp)
+    th1.start()
+    th2.start()
+    # start_new_thread(maintain_servers, (None,))
+    # start_new_thread(loadinp, (None,))
     while True: 
         if len(servers) == 0:
-            sleep(3)
+            time.sleep(5)
+            print('sleeping...')
             continue
-        host = servers[random.randint(0,len(servers)-1]
+        host = servers[random.randint(0,len(servers)-1)]
         port = 12345
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
         s.connect((host,port)) 
         
-        message = str(random.randint(pow(10,args.load-1), pow(10,args.load)))
+        message = str(random.randint(pow(10, load-1), pow(10, load)))
         
         s.send(message.encode('ascii')) 
   
         data = s.recv(1024) 
-  
+        print(load)
         print('Received from the server :',str(data.decode('ascii'))) 
         s.close()
         time.sleep(0.5)
--- monitor.py
+++ monitor.py
@@ -1,13 +1,12 @@
 import libvirt
 import argparse
 import time
-parser = argparse.ArgumentParser()
-parser.add_argument('load', type=int, help='indicator for load on the servers')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument('load', type=int, help='indicator for load on the servers')
+# args = parser.parse_args()
 # print(args.load)
 # print(type(args.load))
-def getCPUtime(dom):
-    return dom.getCPUStats(True)[0]["cpu_time"]
+
 
 domips = {'ubuntu20.04':'192.168.122.100', 'ubuntu20.04-2':'192.168.122.230', 'ubuntu20.04-3':'192.168.122.144'}
 conn = libvirt.open('qemu:///system')
@@ -37,51 +36,46 @@ for x in conn.listAllDomains():
 while True:
 
     # timer = timer + 1
-    
-    for x in conn.listDomainsID():
-        dom = conn.lookupByID(x)
-        if dom == None:
-            print('Failed to find the domain '+x, file=sys.stderr)
-            exit(1)        
-        timer = 0
-        usagelist = list()
+    doms = conn.listAllDomains()
+    prevtimes = [ 0 for x in doms]
+    usagetimes = [ [] for x in doms]
+    n = 10
+    for ii in range(n):
         
-        prevcputime = 0
-        maxhighloadretry = 1
+        for i in range(len(doms)):
+            cputime = doms[i].getCPUStats(True)[0]["cpu_time"]-doms[i].getCPUStats(True)[0]["system_time"]-doms[i].getCPUStats(True)[0]["user_time"]
+            cputime = doms[i].getCPUStats(True)[0]["cpu_time"]
+            
+            cpuutil = (cputime-prevtimes[i])/(2E9)
+            usagetimes[i].append(cpuutil)
+            prevtimes[i] = cputime
+        time.sleep(0.5)
 
-        highloadthreshold = 0.9
-        highloadcount = 0
-        while True:
-            timer = timer + 1
-            if timer >= 1E7:
-                break
-            currcputime = getCPUtime(dom)
-            cpuusage = (currcputime - prevcputime)/(1E7)
-            usagelist.append(cpuusage)
-            prevcputime = currcputime
-        avgusage = sum(usagelist)/maxhighloadretry
-        print(avgusage)
-        # if avgusage > highloadthreshold:
-        #     highloadcount = highloadcount + 1
-        # else :
-            # highloadcount = 0
-        
-        if highloadcount == maxhighloadretry :
-            print(f'High Load from {dom.name()} for the last {maxhighloadretry} times')
-            # Thread(target=startNewVM).start()
-            for x in conn.listAllDomains():
-                if x.isActive() == False:
-                    x.create()        
-                    host = "127.0.0.1"
-                    port = 23456
-                    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
-                    s.connect((host,port)) 
-                    message = domips[x.name()]
-                    s.send(message.encode('ascii')) 
-                    data = s.recv(1024) 
-                    print('Received from the client :',str(data.decode('ascii'))) 
-                    s.close()
-                    break
+    for i in range(len(doms)):
+        avgusage = sum(usagetimes[i])/n
+        print(f'Usage for {doms[i].name()} :: {avgusage}')
+        print()
+    # if avgusage > highloadthreshold:
+    #     highloadcount = highloadcount + 1
+    # else :
+        # highloadcount = 0
+    
+    # if highloadcount == maxhighloadretry :
+    #     print(f'High Load from {dom.name()} for the last {maxhighloadretry} times')
+    #     # Thread(target=startNewVM).start()
+    #     for x in conn.listAllDomains():
+    #         if x.isActive() == False:
+    #             x.create()        
+    #             host = "127.0.0.1"
+    #             port = 23456
+    #             s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+    #             s.connect((host,port)) 
+    #             message = domips[x.name()]
+    #             s.send(message.encode('ascii')) 
+    #             data = s.recv(1024) 
+    #             print('Received from the client :',str(data.decode('ascii'))) 
+    #             s.close()
+    #             break
     time.sleep(1)
 
 conn.close()
